e374dc5c2a891b7dff72c070e3f14f514f3dc25c
Edward-Mercado <edwardmercado149@gmail.com>
2025-10-07 23:14:34 -0400

    filters...

--- app.js
+++ app.js
@@ -1,4 +1,134 @@
+const items = [
+    {
+        "title": "12 Pack Coke Zero",
+        "price": "$14.99",
+        "img_src": "coke-zero-pack.png",
+        "filters": ["DRINKS"],
+    },
+    {
+        "title": "12 Pack Cherry Coke Zero",
+        "price": "$16.99",
+        "img_src": "coke-zero-cherry-pack.png",
+        "filters": ["DRINKS"],
+    },
+    {
+        "title": "Boston Kreme Donut",
+        "price": "$1.79",
+        "img_src": "boston-kreme-donut.png",
+        "filters": ["CHOCOLATE", "DESSERT"],
+    },
+    {
+        "title": "Pastry Frank",
+        "price": "$4.69",
+        "img_src": "pastry-frank.png",
+        "filters": ["SAVORY", "DESSERT"],
+    },
+    {
+        "title": "10 Pack Pocky - Chocolate",
+        "price": "$24.99",
+        "img_src": "pocky.png",
+        "filters": ["CHOCOLATE"],
+    },
+    {
+        "title": "Oishi Salt and Vinegar Chips",
+        "price": "$1.49",
+        "img_src": "oishi-salt-and-vinegar.png",
+        "filters": ["SAVORY"],
+    },
+    {
+        "title": "12 Pack Kinder Bueno",
+        "price": "$11.99",
+        "img_src": "kinder-bueno.png",
+        "filters": ["CHOCOLATE"],
+    },
+    {
+        "title": "30 Pack - Premier Protein Shake Chocolate",
+        "price": "$15.99",
+        "img_src": "premier-protein.png",
+        "filters": ["DRINKS", "CHOCOLATE"],
+    },
+    {
+        "title": "Snack Size Haribo Goldbears",
+        "price": "$1.99",
+        "img_src": "haribo-goldbear.png",
+        "filters": ["CANDY"],
+    },
+    {
+        "title": "Tuxedo Cake",
+        "price": "$18.99",
+        "img_src": "tuxedo-cake.png",
+        "filters": ["DESSERT", "CHOCOLATE"],
+    },
+    {
+        "title": "6 Pack Tate's Bake Shop Cookies",
+        "price": "$24.99",
+        "img_src": "tates-cookies.png",
+        "filters": ["DESSERT"],
+    },
+    { 
+        "title": "6 Pack Fruit by the Foot",
+        "price": "$3.69",
+        "img_src": "fruit-by-the-foot.png",
+        "filters": ["CANDY"],
+    },
+    {
+        "title": "Sausage Egg and Cheese Croissant",
+        "price": "$4.99",
+        "img_src": "sausage-egg-cheese-croissant.png",
+        "filters": ["SAVORY"],
+    },
+    {
+        "title": "Butter Croissant",
+        "price": "$2.79",
+        "img_src": "butter-croissant.png",
+        "filters": ["SAVORY"],
+    },
+    {
+        "title": "10 Pack Pocky - Strawberry",
+        "price": "$24.99",
+        "img_src": "pocky-strawberry.png",
+        "filters": ["DESSERT"],
+    },
+    {
+        "title": "Haribo Twin Snakes",
+        "price": "$3.99",
+        "img_src": "haribo-twin-snakes.webp",
+        "filters": ["CANDY"],
+    },
+    {
+        "title": "6 Pack Mallomars",
+        "price": "$4.49",
+        "img_src": "mallomars.png",
+        "filters": ["CHOCOLATE", "DESSERT"],
+    },
+    {
+        "title": "6 Pack Fruit Roll Ups",
+        "price": "$4.99",
+        "img_src": "fruit-roll-up.png",
+        "filters": ["CANDY"],
+    },
+    {
+        "title": "CARDSTOCK",
+        "price": "$0.09",
+        "img_src": "cardstock.png",
+        "filters": [],
+    },
+    {
+        "title": "Hershey's Cookies and Cream Bar",
+        "price": "$2.99",
+        "img_src": "hersheys-cnc.png",
+        "filters": ["CHOCOLATE", "DESSERT"],
+    },
+]
+
+const filterButtons = document.querySelectorAll(".filter-bar__button");
+
+const filtersActive = []
+
 function inject_item(item) {
+    const DOMSelectors = {
+        display: document.querySelector(".container")
+    }
     DOMSelectors.display.insertAdjacentHTML(
     "afterbegin",
     `
@@ -18,113 +148,46 @@ function inject_item(item) {
     </div>
     `
     )
-} // the first argument is position, second is an f string w all your stuff
+} 
+items.forEach(item => inject_item(item));
 
-// make a list of all my stuff
-// title img price
-// loop through the array and then run that
+function applyFilter() {
+    // use filtersActive to filter items
+    const marketItems = document.querySelectorAll(".market-item");
+    marketItems.forEach(item => item.remove());
+    filtersActive.sort();
+    if (filtersActive.length === 0) {
+        items.forEach(item => inject_item(item));
+    }
+    else {
+        filtersActive.forEach(filter => {
+            items.forEach(
+                item => {
+                    if (item.filters.includes(filter)) {
+                        inject_item(item);
+                    }
+                }
+            )
+        })
+    }
+} // nesting code is a passion
 
-const items = [
-    {
-        "title": "12 Pack Coke Zero",
-        "price": "$14.99",
-        "img_src": "coke-zero-pack.png",
-    },
-    {
-        "title": "12 Pack Cherry Coke Zero",
-        "price": "$16.99",
-        "img_src": "coke-zero-cherry-pack.png",
-    },
-    {
-        "title": "Boston Kreme Donut",
-        "price": "$1.79",
-        "img_src": "boston-kreme-donut.png",
-    },
-    {
-        "title": "Pastry Frank",
-        "price": "$4.69",
-        "img_src": "pastry-frank.png",
-    },
-    {
-        "title": "10 Pack Pocky - Chocolate",
-        "price": "$24.99",
-        "img_src": "pocky.png",
-    },
-    {
-        "title": "Oishi Salt and Vinegar Chips",
-        "price": "$1.49",
-        "img_src": "oishi-salt-and-vinegar.png",
-    },
-    {
-        "title": "12 Pack Kinder Bueno",
-        "price": "$11.99",
-        "img_src": "kinder-bueno.png",
-    },
-    {
-        "title": "30 Pack - Premier Protein Shake Chocolate",
-        "price": "$15.99",
-        "img_src": "premier-protein.png",
-    },
-    {
-        "title": "Snack Size Haribo Goldbears",
-        "price": "$1.99",
-        "img_src": "haribo-goldbear.png",
-    },
-    {
-        "title": "Tuxedo Cake",
-        "price": "$18.99",
-        "img_src": "tuxedo-cake.png",
-    },
-    {
-        "title": "6 Pack Tate's Bake Shop Cookies",
-        "price": "$24.99",
-        "img_src": "tates-cookies.png",
-    },
-    { 
-        "title": "6 Pack Fruit by the Foot",
-        "price": "$3.69",
-        "img_src": "fruit-by-the-foot.png",
-    },
-    { // hi
-        "title": "Sausage Egg and Cheese Croissant",
-        "price": "$4.99",
-        "img_src": "sausage-egg-cheese-crossaint.png",
-    },
-    {
-        "title": "Butter Croissant",
-        "price": "$2.79",
-        "img_src": "butter-croissant.png",
-    },
-    {
-        "title": "10 Pack Pocky - Strawberry",
-        "price": "$24.99",
-        "img_src": "pocky-strawberry.png",
-    },
-    {
-        "title": "Haribo Twin Snakes",
-        "price": "$3.99",
-        "img_src": "haribo-twin-snakes.webp",
-    },
-    {
-        "title": "6 Pack Mallomars",
-        "price": "$4.49",
-        "img_src": "mallomars.png",
-    },
-    {
-        "title": "6 Pack Fruit Roll Ups",
-        "price": "$4.99",
-        "img_src": "fruit-roll-up.png",
-    },
-    {
-        "title": "CARDSTOCK",
-        "price": "$0.09",
-        "img_src": "cardstock.png",
-    },
-    {
-        "title": "Hershey's Cookies and Cream Bar",
-        "price": "$2.99",
-        "img_src": "hersheys-cnc.png",
-    },
-]
+function changeFilterButton(button) {
+    if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        filtersActive.splice(filtersActive.indexOf(button.textContent), 1);
+      } else {
+        button.classList.add("active");
+        filtersActive.push(button.textContent);
+      }
+      applyFilter();
+}
 
-console.log(items.length);
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        changeFilterButton(button)
+    }
+)}
+)
+
+console.log(filterButtons)
